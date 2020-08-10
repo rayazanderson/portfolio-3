794a3a030fb6697d3bc0f3823359651b430563b0
Raya Anderson <rayazand@gmail.com>
2020-08-09 23:32:35 -0700

    rain banner

--- src/components/Global/banner.js
+++ src/components/Global/banner.js
@@ -2,20 +2,39 @@ import React from "react"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 const Banner = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300, quality: 100) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+  //       childImageSharp {
+  //         fluid(maxWidth: 300, quality: 100) {
+  //           ...GatsbyImageSharpFluid
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <div className="banner">
+      <div className="layer-1"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
+      <div className="rain-drop"></div>
       <div className="container">
         <div className="row">
           <div className="main-text"> Raya Anderson </div>
